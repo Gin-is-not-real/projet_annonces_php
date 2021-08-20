5ee3ce654412f391a9cd230cb77c463b0918c406
Gin-is-not-real <ninapariat@live.fr>
2021-08-20 09:37:33 +0200

    commit before test merge

---
+++ Manager/ImageManager.php
@@ -0,0 +1,19 @@
+<?php 
+require_once 'DatabaseManager.php';
+
+class ImageManager extends DatabaseManager {
+    public function add($image) {
+        var_dump($image);
+        try {
+            $entry = $this->pdo->prepare("INSERT INTO $this->tablename (id, filename, offer_id) VALUES (:id, :filename, :offer_id)");
+            $affectedLines = $entry->execute(array(
+                'id' => date('hismd'),
+                'filename' => $image['filename'],
+                'offer_id' => $image['offer_id']
+            ));
+        } catch (Exception $e) {
+            die('Error while try add an image: ' . $e->getMessage());
+        }
+    }
+    //line for test git
+}
